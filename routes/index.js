48d97163b1fb05406b85db6a98e4e415d801934e
Pawel <pawel.html@gmail.com>
2017-10-27 11:57:39 +0200

    added CSRF protection

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 var Product = require('../models/product');
 
+// sessions, safty: 
+var csrf = require('csurf');
+var csrfProtection = csrf();
+router.use(csrfProtection);
+
 /* GET home page. */
-router.get('/', function (req, res, next){
+router.get('/', function (req, res){
   
   //connect to db, and sending data to front-end
   Product.find(function(err, docs){
-    
     var productChunks = [];
     var chunk = 3;
-
     for(let i = 0; i<docs.length; i+=chunk){
       productChunks.push(docs.slice(i, i+chunk));
     }
@@ -18,5 +21,16 @@ router.get('/', function (req, res, next){
   });
 
 });
+
+/* GET signup page. */
+router.get('/user/signup', function(req, res){
+  res.render('user/signup', {csrfToken: req.csrfToken()});
+});
+
+/* POST signup page. */
+router.post('/user/signup', function(req, res){
+  res.redirect('/');
+})
+
 
 module.exports = router;
